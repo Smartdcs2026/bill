6dc985503d881ce33eb6bc6eccced0d15127bc5a
SKDC_SMART <somchaibutphon@gmail.com>
2026-07-08 13:42:17 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,58 @@
+/************************************************************
+ * sw.js
+ * Receipt OCR System Service Worker
+ ************************************************************/
+
+const CACHE_NAME = 'receipt-ocr-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      return cached || fetch(req).then(res => {
+        const copy = res.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(req, copy);
+        });
+
+        return res;
+      });
+    })
+  );
+});
